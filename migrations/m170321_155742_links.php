b92950a03dd0ddb18a41de3258dbe9dd0b5adadf
Zhiganov Timofey <flashzavod@gmail.com>
2017-03-21 19:03:48 +0300

    Развернул фреймворк

---
+++ migrations/m170321_155742_links.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170321_155742_links extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170321_155742_links cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
